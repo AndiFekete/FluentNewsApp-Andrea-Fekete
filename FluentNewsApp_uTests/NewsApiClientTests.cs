4614d1b4c7b8642578bef7e1f1733938b98018c9
AndiFekete <andikaa@gmail.com>
2025-06-20 20:57:11 +0200

    Unit test project

---
+++ FluentNewsApp_uTests/NewsApiClientTests.cs
@@ -0,0 +1,17 @@
+﻿namespace FluentNewsApp_uTests
+{
+    public class NewsApiClientTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+            //mock stuff
+        }
+
+        [Test]
+        public async Task Test()
+        {
+            Assert.Pass("");
+        }
+    }
+}
