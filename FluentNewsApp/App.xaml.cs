8f5fbf14f8d1e2323c2915b3e04201fb1c348d9b
AndiFekete <andikaa@gmail.com>
2025-06-20 20:55:56 +0200

    creared NewsApiClient class and added needed services

--- FluentNewsApp/App.xaml.cs
+++ FluentNewsApp/App.xaml.cs
@@ -1,5 +1,8 @@
-﻿using System.Configuration;
-using System.Data;
+﻿using FluentNewsApp.WebCalls;
+using Microsoft.Extensions.DependencyInjection;
+using System.Configuration;
+using System.Net.Http;
+using System.Printing;
 using System.Windows;
 
 namespace FluentNewsApp
@@ -9,6 +12,30 @@ namespace FluentNewsApp
     /// </summary>
     public partial class App : Application
     {
+        IServiceCollection _services = new ServiceCollection();
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+            ConfigureServices();
+            // Additional startup logic can go here
+        }
+
+        private void ConfigureServices()
+        {
+            var userApiKey = ConfigurationManager.AppSettings["userApiKey"];
+            if (string.IsNullOrEmpty(userApiKey))
+            {
+                throw new ConfigurationErrorsException("The 'userApiKey' setting is missing or empty in the application configuration.");
+            }   //TODO maybe ask for user api key if not set
+
+            _services.AddHttpClient("newsAPI", httpClient =>
+            {
+                httpClient.DefaultRequestHeaders.Authorization = 
+                    new System.Net.Http.Headers.AuthenticationHeaderValue(userApiKey);
+            });
+            _services.AddSingleton<NewsApiClient>();
+        }
     }
 
 }
---
+++ FluentNewsApp/WebCalls/NewsApiClient.cs
@@ -0,0 +1,31 @@
+﻿using System.Net.Http;
+
+namespace FluentNewsApp.WebCalls
+{
+    public sealed class NewsApiClient
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public NewsApiClient(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<string> GetTopNewsAsync()
+        {
+            var url = "https://newsapi.org/v2/top-headlines?country=us";
+            return await GetAsync(url).ContinueWith(response => response.Result.Content.ReadAsStringAsync().Result);
+        }
+
+        private async Task<HttpResponseMessage> GetAsync(string url)
+        {
+            var client = _httpClientFactory.CreateClient("newsAPI");
+            var response = await client.GetAsync(url);
+            if (response.IsSuccessStatusCode)
+            {
+                return response;
+            }
+            throw new HttpRequestException($"Error fetching news: {response.ReasonPhrase}");
+        }
+    }
+}
