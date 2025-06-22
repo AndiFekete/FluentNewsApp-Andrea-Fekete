374870fab56f40513c102df31f1de5c202d3255a
AndiFekete <andikaa@gmail.com>
2025-06-22 10:54:20 +0200

    ReyalCommand impl

---
+++ FluentNewsApp/Commands/AsyncCommandBase.cs
@@ -0,0 +1,53 @@
+﻿using System.Windows.Input;
+
+namespace FluentNewsApp.Commands
+{
+    public abstract class AsyncCommandBase : ICommand
+    {
+        private bool _isExecuting;
+        private readonly Action<Exception> _onException;
+
+        public bool IsExecuting
+        {
+            get => _isExecuting;
+            protected set
+            {
+                if (_isExecuting != value)
+                {
+                    _isExecuting = value;
+                    CanExecuteChanged?.Invoke(this, EventArgs.Empty);
+                }
+            }
+        }
+
+        public event EventHandler CanExecuteChanged;
+
+        public AsyncCommandBase(Action<Exception> onException)
+        {
+            _onException = onException;
+        }
+
+        public virtual bool CanExecute(object? parameter)
+        {
+            return !IsExecuting;
+        }
+
+        public async void Execute(object? parameter)
+        {
+            IsExecuting = true;
+
+            try
+            {
+                await ExecuteAsync(parameter);
+            }
+            catch (Exception ex)
+            {
+                _onException?.Invoke(ex);
+            }
+
+            IsExecuting = false;
+        }
+
+        protected abstract Task ExecuteAsync(object? parameter);
+    }
+}
---
+++ FluentNewsApp/Commands/RelayCommand.cs
@@ -0,0 +1,19 @@
+﻿using FluentNewsApp.ViewModels;
+
+namespace FluentNewsApp.Commands
+{
+    public class RelayCommand : AsyncCommandBase
+    {
+        private readonly Func<Task> _execute;
+
+        public RelayCommand(Func<Task> execute, Action<Exception> onException) : base(onException)
+        {
+            _execute = execute ?? throw new ArgumentNullException(nameof(execute));
+        }
+
+        protected override async Task ExecuteAsync(object? parameter)
+        {
+            await _execute();
+        }
+    }
+}
